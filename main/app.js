0ab2ce07f8a863f87314120a5c55025a1ca37411
Oana Dragomirescu <dragomirescuoana18@stud.ase.ro>
2020-11-24 21:05:56 +0200

    Dragomirescu Oana, 1080, seria C - tema 2

--- main/app.js
+++ main/app.js
@@ -1,5 +1,18 @@
 function addTokens(input, tokens){
-    
+    if (typeof input !== 'string' )
+    throw "Invalid input"
+    if(input.length<6)
+    throw "Input should have at least 6 characters"
+    tokens.forEach(t =>{
+        if(typeof t.tokenName !== 'string'){
+            console.log(t.tokenName)
+        throw "Invalid array format"
+        }
+        });
+    if(!input.includes('...'))
+    return input
+        else
+        return input.replace('...', '${'+ tokens[0].tokenName +'}')  
 }
 
 const app = {
